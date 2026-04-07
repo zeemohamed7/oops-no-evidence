a6e2fe4b704c7eaf129f9467205947f2bc734cb7
zeemohamed7 <zainabshmohamed7@gmail.com>
2026-04-07 12:30:05 +0300

    Add GuardPatrol.cs logic

---
+++ Assets/Scripts/GuardPatrol.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    // Waypoints
+    public Transform[] waypoints;
+    private NavMeshAgent agent;
+    private Vector3 target;
+    private int waypointIndex;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        UpdateDestination();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // If reached, update]
+        if (Vector3.Distance(transform.position, target) < 1)
+        {
+            IterateWaypointIndex();
+            UpdateDestination();
+        }
+    }
+
+    // Update target position and set agent's destination to that target
+    private void UpdateDestination()
+    {
+        target = waypoints[waypointIndex].position;
+        agent.SetDestination(target);
+    }
+
+    // Increase waypoint index to go to the next one
+    private void IterateWaypointIndex()
+    {
+        waypointIndex++;
+        if (waypointIndex >= waypoints.Length) waypointIndex = 0;
+    }
+}
