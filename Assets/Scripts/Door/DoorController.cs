618867235fbf4eb73b23c657ea3f50f0e103d925
zeemohamed7 <zainabshmohamed7@gmail.com>
2026-05-03 17:09:46 +0300

    add doors and animation to level1

---
+++ Assets/Scripts/Door/DoorController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            anim.SetBool("isOpen", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            anim.SetBool("isOpen", false);
+        }
+    }
+}
