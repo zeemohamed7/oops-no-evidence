733388a9449e067da53dbc2afd98b5fd76eeabd4
haaljafen <hajaraljafen06@gmail.com>
2026-05-10 17:33:33 +0300

    timer starts when the players are outside the van

--- Assets/Scripts/GameLoop/GameManager.cs
+++ Assets/Scripts/GameLoop/GameManager.cs
@@ -5,7 +5,7 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
-    public enum GameState { Playing, Won, Lost }
+    public enum GameState { Waiting, Playing, Won, Lost }
 
     [Header("Timer")]
     public float levelDuration = 360f; // 6 minutes default
@@ -30,8 +30,7 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         TimeRemaining = levelDuration;
-        State = GameState.Playing;
-        
+        State = GameState.Waiting;
     }
 
     private void OnEnable()
