c10960f77ed2945a3c2eaa08aedbbbbc1b3d0725
haaljafen <hajaraljafen29@gmail.com>
2026-05-17 21:14:35 +0300

    conflicts

--- Assets/Scripts/GameLoop/GameManager.cs
+++ Assets/Scripts/GameLoop/GameManager.cs
@@ -8,7 +8,6 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance;
 
     public enum GameState { Waiting, Playing, Won, Lost }
-    public enum GameState { Waiting, Playing, Won, Lost }
 
     [Header("Timer")]
     public float levelDuration = 360f;
